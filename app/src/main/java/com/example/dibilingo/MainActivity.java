da83f2e5d470ca9d197d70177c1809caac3fc6d0
Karina Romanova <twidocka@yandex.ru>
2020-12-02 09:13:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dibilingo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dibilingo;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/dibilingo/first_lvl.java
@@ -0,0 +1,14 @@
+package com.example.dibilingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class first_lvl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_lvl);
+    }
+}
