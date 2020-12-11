ed0ef554f699c09e5e5d6ebe63a778c7cccfefa0
Karina Romanova <twidocka@yandex.ru>
2020-12-11 17:35:24 +0300

    1st level view

--- app/src/main/java/com/example/dibilingo/MainActivity.java
+++ app/src/main/java/com/example/dibilingo/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+
+
 public class MainActivity extends Activity {
 
 
@@ -13,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
