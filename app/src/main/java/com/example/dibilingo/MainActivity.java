70ea5e4a7e0447acddfebdf5c6d55be0168b1c3b
Karina Romanova <twidocka@yandex.ru>
2020-12-15 14:05:21 +0300

    dfgh

--- app/src/main/java/com/example/dibilingo/MainActivity.java
+++ app/src/main/java/com/example/dibilingo/MainActivity.java
@@ -41,7 +41,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.StackView;
 
-import com.wenchao.cardstack.CardStack;
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,26 +60,68 @@ public class MainActivity extends AppCompatActivity {
     private StackView stackView;
     private Button buttonPrevious;
     private Button buttonNext;
-
+CardContainer mCardContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.stackView = (StackView) findViewById(R.id.stackView);
-
-
-        List<StackItem> items = new ArrayList<StackItem>();
-
-        for(String imageName: IMAGE_NAMES) {
-            items.add(new StackItem(imageName+".png", imageName));
-        }
-
-        FrameAdapter adapt = new FrameAdapter(this, R.layout.stack_item, items);
-        stackView.setAdapter(adapt);
-        stackView.setOnTouchListener();
-
+        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+        mCardContainer.setOrientation(Orientations.Orientation.Ordered);
+        CardModel card = new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1);
+//        relativeLayout.setOnTouchListener(new View.OnTouchListener(){
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event){
+//                x=event.getX();
+//                y=event.getY();
+//                if(event.getAction()==MotionEvent.ACTION_MOVE){
+//                    if(x<relativeLayout.getWidth()/2){
+//                    stackView.setPivotX(0);
+//                    stackView.setPivotY(stackView.getHeight());}
+//                    if(x>=relativeLayout.getWidth()/2){
+//                        stackView.setPivotX(stackView.getWidth());
+//                        stackView.setPivotY(stackView.getHeight());}
+//                    stackView.setRotation((x-relativeLayout.getWidth()/2)/6);
+//                }
+//                if(event.getAction()==MotionEvent.ACTION_UP){
+//                    if (stackView.getRotation()>=10){
+//                        RotateAnimation rotate = new RotateAnimation(0, 100, Animation.RELATIVE_TO_SELF,
+//                        1f,  Animation.RELATIVE_TO_SELF, 1f);
+//                        stackView.setAlpha(1f);
+//                        stackView.setVisibility(View.VISIBLE);
+//                        stackView.animate()
+//                                .alpha(0f)
+//                                .setDuration(400)
+//                                .setListener(null);
+//                        rotate.setDuration(500);
+//                        stackView.startAnimation(rotate);
+//                relativeLayout.removeView(stackView);}
+//                    if (stackView.getRotation()<=-10){
+//                        RotateAnimation rotate = new RotateAnimation(0, -100, Animation.RELATIVE_TO_SELF,
+//                                0f,  Animation.RELATIVE_TO_SELF, 1f);
+//                        frameLayout.setAlpha(1f);
+//                        frameLayout.setVisibility(View.VISIBLE);
+//                        frameLayout.animate()
+//                                .alpha(0f)
+//                                .setDuration(400)
+//                                .setListener(null);
+//                        rotate.setDuration(500);
+//                        frameLayout.startAnimation(rotate);
+//                        relativeLayout.removeView(frameLayout);}
+//                    else{
+//                        frameLayout.setRotation(frameLayout.getRotation());
+//                        frameLayout.animate()
+//                                .rotation(0)
+//
+//                                .setListener(null);
+//
+//
+//                    }
+//                }
+//                return true;
+//            }
+//        });
 
 
     }
